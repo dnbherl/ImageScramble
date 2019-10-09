36a4dff141bb54b7741006971f42de2b9781c471
DNBHERL <54248060+githubspidey@users.noreply.github.com>
2019-10-09 03:56:48 -0400

    Add files via upload

---
+++ ImageScrambler/ImageScrambler/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿using System.Windows.Forms;
+
+namespace ImageScrambler
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.picMain = new System.Windows.Forms.PictureBox();
+            this.btnScramble = new System.Windows.Forms.Button();
+            this.btnUnscramble = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassphrase = new System.Windows.Forms.TextBox();
+            this.progBar = new System.Windows.Forms.ProgressBar();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(540, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadImageToolStripMenuItem,
+            this.saveImageToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadImageToolStripMenuItem
+            // 
+            this.loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
+            this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.loadImageToolStripMenuItem.Text = "Load Image";
+            this.loadImageToolStripMenuItem.Click += new System.EventHandler(this.LoadImageToolStripMenuItem_Click);
+            // 
+            // saveImageToolStripMenuItem
+            // 
+            this.saveImageToolStripMenuItem.Name = "saveImageToolStripMenuItem";
+            this.saveImageToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.saveImageToolStripMenuItem.Text = "Save Image";
+            this.saveImageToolStripMenuItem.Click += new System.EventHandler(this.SaveImageToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // picMain
+            // 
+            this.picMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picMain.Location = new System.Drawing.Point(12, 36);
+            this.picMain.MaximumSize = new System.Drawing.Size(350, 350);
+            this.picMain.Name = "picMain";
+            this.picMain.Size = new System.Drawing.Size(350, 350);
+            this.picMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMain.TabIndex = 1;
+            this.picMain.TabStop = false;
+            this.picMain.WaitOnLoad = true;
+            // 
+            // btnScramble
+            // 
+            this.btnScramble.Location = new System.Drawing.Point(372, 81);
+            this.btnScramble.Name = "btnScramble";
+            this.btnScramble.Size = new System.Drawing.Size(75, 23);
+            this.btnScramble.TabIndex = 2;
+            this.btnScramble.Text = "Scramble";
+            this.btnScramble.UseVisualStyleBackColor = true;
+            this.btnScramble.Click += new System.EventHandler(this.BtnScramble_Click);
+            // 
+            // btnUnscramble
+            // 
+            this.btnUnscramble.Location = new System.Drawing.Point(453, 81);
+            this.btnUnscramble.Name = "btnUnscramble";
+            this.btnUnscramble.Size = new System.Drawing.Size(75, 23);
+            this.btnUnscramble.TabIndex = 3;
+            this.btnUnscramble.Text = "Unscramble";
+            this.btnUnscramble.UseVisualStyleBackColor = true;
+            this.btnUnscramble.Click += new System.EventHandler(this.BtnUnscramble_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(369, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Passphrase:";
+            // 
+            // txtPassphrase
+            // 
+            this.txtPassphrase.Location = new System.Drawing.Point(372, 55);
+            this.txtPassphrase.Name = "txtPassphrase";
+            this.txtPassphrase.Size = new System.Drawing.Size(156, 20);
+            this.txtPassphrase.TabIndex = 6;
+            // 
+            // progBar
+            // 
+            this.progBar.Location = new System.Drawing.Point(368, 363);
+            this.progBar.Name = "progBar";
+            this.progBar.Size = new System.Drawing.Size(160, 23);
+            this.progBar.TabIndex = 7;
+            // 
+            // frmMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 393);
+            this.Controls.Add(this.progBar);
+            this.Controls.Add(this.txtPassphrase);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUnscramble);
+            this.Controls.Add(this.btnScramble);
+            this.Controls.Add(this.picMain);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ImageScramble v1.0";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMain_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmMain_DragEnter);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.PictureBox picMain;
+        private System.Windows.Forms.Button btnScramble;
+        private System.Windows.Forms.Button btnUnscramble;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassphrase;
+        private System.Windows.Forms.ProgressBar progBar;
+    }
+}
+
---
+++ ImageScrambler/ImageScrambler/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImageScrambler
+{
+    public partial class frmMain : Form
+    {
+
+        public Bitmap thisImage = null;
+        
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dg = new OpenFileDialog();            
+            if (dg.ShowDialog() == DialogResult.OK)
+            {
+                if (dg.OpenFile() != null)
+                {
+                    LoadImage(dg.FileName);
+                }
+            }
+        }
+
+        private void LoadImage(string filename)
+        {
+            thisImage = new Bitmap(filename);
+            picMain.Width = 350;
+            picMain.Height = 350;
+            picMain.ImageLocation = filename;
+
+            if (thisImage.Height > thisImage.Width)
+            {
+                picMain.Width = Convert.ToInt32(Convert.ToDecimal(thisImage.Width) / Convert.ToDecimal(thisImage.Height) * 350);
+            }
+            if (thisImage.Width > thisImage.Height)
+            {
+                picMain.Height = Convert.ToInt32(Convert.ToDecimal(thisImage.Height) / Convert.ToDecimal(thisImage.Width) * 350);
+            }
+        }
+        
+
+        private void SaveImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (thisImage == null) return;
+            SaveFileDialog dg = new SaveFileDialog();
+            dg.Filter = "PNG (*.png)|*.png";
+            dg.DefaultExt = "png";
+            dg.AddExtension = true;
+            if (dg.ShowDialog() == DialogResult.OK)
+            {
+                if (dg.FileName != null)
+                {
+                    thisImage.Save(dg.FileName, ImageFormat.Png);
+                }
+            }
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnScramble_Click(object sender, EventArgs e)
+        {
+            if (thisImage != null) ScrambleImage();
+        }
+
+        private void BtnUnscramble_Click(object sender, EventArgs e)
+        {
+            if (thisImage != null) UnscrambleImage();
+        }
+
+        private void ScrambleImage()
+        {
+            int tarX;
+            int tarY;
+            long cnt = 0;
+            string hashval = CreateHashString(txtPassphrase.Text);
+            Color newCol = new Color();
+            Color newCol2 = new Color();
+            progBar.Maximum = thisImage.Height * thisImage.Width * 2;
+            progBar.Value = 0;
+            this.Enabled = false;
+            for (int y = 0; y < thisImage.Height; y++)
+            {
+                for(int x = 0; x< thisImage.Width; x++)
+                {
+                    cnt += 1;
+                    progBar.Value += 1;
+                    int hv = (int)hashval[(int)(cnt % (long)(hashval.Length))];
+                    tarX = (int)((cnt*hv^23) % thisImage.Width);
+                    tarY = (int)((cnt*hv^23) % thisImage.Height);
+                    Color swapPixel = thisImage.GetPixel(tarX, tarY);
+                    Color tmpPixel = thisImage.GetPixel(x, y);
+                    switch (cnt % 3)
+                    {
+                        case 0:
+                            newCol = Color.FromArgb(swapPixel.R, swapPixel.G, swapPixel.B);
+                            newCol2 = Color.FromArgb(tmpPixel.R, tmpPixel.G, tmpPixel.B);
+                            break;
+                        case 1:
+                            newCol = Color.FromArgb(swapPixel.B, swapPixel.R, swapPixel.G);
+                            newCol2 = Color.FromArgb(tmpPixel.B, tmpPixel.R, tmpPixel.G);
+                            break;
+                        case 2:
+                            newCol = Color.FromArgb(swapPixel.B, swapPixel.G, swapPixel.R);
+                            newCol2 = Color.FromArgb(tmpPixel.B, tmpPixel.G, tmpPixel.R);
+                            break;
+                    }
+                    thisImage.SetPixel(x, y, newCol);
+                    thisImage.SetPixel(tarX, tarY, newCol2);
+                }
+            }
+            cnt = 0;
+            for (int x = 0; x < thisImage.Width; x++)
+            {
+                for (int y = 0; y < thisImage.Height; y++)
+                {
+                    cnt += 1;
+                    progBar.Value += 1;
+                    int hv = (int)hashval[(int)(cnt % (long)(hashval.Length))];
+                    tarX = (int)((cnt * hv ^ 19) % thisImage.Width);
+                    tarY = (int)((cnt * hv ^ 19) % thisImage.Height);
+                    Color swapPixel = thisImage.GetPixel(tarX, tarY);
+                    Color tmpPixel = thisImage.GetPixel(x, y);
+                    switch (cnt % 3)
+                    {
+                        case 0:
+                            newCol = Color.FromArgb(swapPixel.R, swapPixel.G, swapPixel.B);
+                            newCol2 = Color.FromArgb(tmpPixel.R, tmpPixel.G, tmpPixel.B);
+                            break;
+                        case 1:
+                            newCol = Color.FromArgb(swapPixel.B, swapPixel.R, swapPixel.G);
+                            newCol2 = Color.FromArgb(tmpPixel.B, tmpPixel.R, tmpPixel.G);
+                            break;
+                        case 2:
+                            newCol = Color.FromArgb(swapPixel.B, swapPixel.G, swapPixel.R);
+                            newCol2 = Color.FromArgb(tmpPixel.B, tmpPixel.G, tmpPixel.R);
+                            break;
+                    }
+                    thisImage.SetPixel(x, y, newCol);
+                    thisImage.SetPixel(tarX, tarY, newCol2);
+                }
+            }
+            picMain.Image = thisImage;
+            progBar.Value = 0;
+            this.Enabled = true;
+        }
+
+        private void UnscrambleImage()
+        {
+            int tarX;
+            int tarY;
+            Color newCol = new Color();
+            Color newCol2 = new Color();
+            long cnt = thisImage.Width * thisImage.Height;
+            string hashval = CreateHashString(txtPassphrase.Text);
+            progBar.Maximum = thisImage.Height * thisImage.Width * 2;
+            progBar.Value = 0;
+            this.Enabled = false;
+            for (int x = thisImage.Width - 1; x >= 0; x--)
+            {
+                for (int y = thisImage.Height - 1; y >= 0; y--)
+                {
+                    progBar.Value += 1;
+                    int hv = (int)hashval[(int)(cnt % (long)(hashval.Length))];
+                    tarX = (int)((cnt * hv ^ 19) % thisImage.Width);
+                    tarY = (int)((cnt * hv ^ 19) % thisImage.Height);
+                    Color swapPixel = thisImage.GetPixel(tarX, tarY);
+                    Color tmpPixel = thisImage.GetPixel(x, y);
+                    switch (cnt % 3)
+                    {
+                        case 0:
+                            newCol = Color.FromArgb(swapPixel.R, swapPixel.G, swapPixel.B);
+                            newCol2 = Color.FromArgb(tmpPixel.R, tmpPixel.G, tmpPixel.B);
+                            break;
+                        case 1:
+                            newCol = Color.FromArgb(swapPixel.G, swapPixel.B, swapPixel.R);
+                            newCol2 = Color.FromArgb(tmpPixel.G, tmpPixel.B, tmpPixel.R);
+                            break;
+                        case 2:
+                            newCol = Color.FromArgb(swapPixel.B, swapPixel.G, swapPixel.R);
+                            newCol2 = Color.FromArgb(tmpPixel.B, tmpPixel.G, tmpPixel.R);
+                            break;
+                    }
+                    thisImage.SetPixel(x, y, newCol);
+                    thisImage.SetPixel(tarX, tarY, newCol2);
+                    cnt -= 1;
+                }
+            }
+            cnt = thisImage.Width * thisImage.Height;
+            for (int y = thisImage.Height - 1; y >= 0; y--)
+            {
+                for (int x = thisImage.Width - 1; x >= 0; x--)
+                {
+                    progBar.Value += 1;
+                    int hv = (int)hashval[(int)(cnt % (long)(hashval.Length))];
+                    tarX = (int)((cnt * hv ^ 23) % thisImage.Width);
+                    tarY = (int)((cnt * hv ^ 23) % thisImage.Height);
+                    Color swapPixel = thisImage.GetPixel(tarX, tarY);
+                    Color tmpPixel = thisImage.GetPixel(x, y);
+                    switch (cnt % 3)
+                    {
+                        case 0:
+                            newCol = Color.FromArgb(swapPixel.R, swapPixel.G, swapPixel.B);
+                            newCol2 = Color.FromArgb(tmpPixel.R, tmpPixel.G, tmpPixel.B);
+                            break;
+                        case 1:
+                            newCol = Color.FromArgb(swapPixel.G, swapPixel.B, swapPixel.R);
+                            newCol2 = Color.FromArgb(tmpPixel.G, tmpPixel.B, tmpPixel.R);
+                            break;
+                        case 2:
+                            newCol = Color.FromArgb(swapPixel.B, swapPixel.G, swapPixel.R);
+                            newCol2 = Color.FromArgb(tmpPixel.B, tmpPixel.G, tmpPixel.R);
+                            break;
+                    }
+                    thisImage.SetPixel(x, y, newCol);
+                    thisImage.SetPixel(tarX, tarY, newCol2);
+                    cnt -= 1;
+                }
+            }
+            picMain.Image = thisImage;
+            progBar.Value = 0;
+            this.Enabled = true;
+        }
+
+        public static String CreateHashString(string value)
+        {
+            String ret = "";
+            string h = sha256_hash(value);
+            for (int i = 0; i < h.Length; i++)
+            {
+                ret += sha256_hash(h[i].ToString());
+            }
+            return ret;
+        }
+
+        public static String sha256_hash(String value)
+        {
+            StringBuilder Sb = new StringBuilder();
+
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                Byte[] result = hash.ComputeHash(enc.GetBytes(value));
+
+                foreach (Byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+
+            return Sb.ToString();
+        }
+
+        private void frmMain_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void frmMain_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            LoadImage(files[0]);
+        }
+
+    }
+}
